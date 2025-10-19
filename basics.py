1ae591705e305fa917c700557efba02148ea0c58
gpmclaug <gpmclaug@uci.edu>
2025-10-19 13:22:33 -0700

    Second commit

---
+++ basics.py
@@ -0,0 +1,18 @@
+a=10
+b=3.14
+c="Python"
+
+print(a,b,c)
+
+def square(x):
+    return x*x
+print(square(5))
+
+nums= [1, 2, 3, 4]
+for n in nums:
+    print(n**2)
+
+    import numpy as np
+    arr = np.array([1, 2, 3])
+    print(arr*2)
+
